e817c0f4d849fd79642ae45c861a13f8e3308645
Ryan Miller <rnmiller@ucsc.edu>
2023-03-07 17:03:30 -0800

    setup

---
+++ pa7/DictionaryTest.cpp
@@ -0,0 +1,7 @@
+/********************************************************************************* 
+* Ryan Miller, rnmiller
+* 2023 Winter CSE101 PA7 
+* DictionaryTest.cpp 
+* Test client for Dictionary ADT
+* If no errors are raised and all memory is freed then Dictionary ADT worked correctly 
+*********************************************************************************/ 
---
+++ pa7/Order.cpp
@@ -0,0 +1,7 @@
+/********************************************************************************* 
+* Ryan Miller, rnmiller 
+* 2023 Winter CSE101 PA6 
+* Order.c
+* Order program that creates and prints a Dictionary
+* Usage: Order <input file> <output file>
+*********************************************************************************/ 
